dc69e6ad85a0c8868f3ac54b5b6f367a72a93997
donaldzch <donaldzch@gmail.com>
2014-04-11 15:58:56 +0800

    Initial commit from local git

---
+++ Hello World/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Donald Zhu on 4/11/14.
+//  Copyright (c) 2014 Donald. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
